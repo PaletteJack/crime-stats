4ba4839e0ac9e4024a916f80f8681223843aa9c1
PaletteJack <barbozalouie@gmail.com>
2023-10-23 14:26:04 -0700

    Added more data graphs. still exploring data.

--- load_data.py
+++ load_data.py
@@ -2,7 +2,6 @@ import sqlite3
 import pandas as pd
 
 conn = sqlite3.connect('sqlite3.db')
-cols = ['Case Number', 'Date', 'Block', 'IUCR', 'Primary Type', 'Description', 'Location Description', 'Arrest', 'Domestic', 'FBI Code', 'Year', 'Updated On']
 chunk_size = 50000
-for chunk in pd.read_csv('Crimes_2001_to_Present.csv', chunksize=chunk_size, usecols=cols):
+for chunk in pd.read_csv('Crimes_2001_to_Present.csv', chunksize=chunk_size):
     chunk.to_sql('crime_data', conn, if_exists='append', index=False)
